d49df1a44b2381c53f7f18f5aba465b3bf9934f9
Leonel <8210127@estg.ipp.pt>
2024-01-18 20:25:34 +0000

    Updates

--- compile.sh
+++ compile.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 # Compilação dos arquivos .java e colocação no diretório de saída com o JAR do JFreeChart no classpath
-javac -cp ./lib/jfreechart.jar -d ./SatelliteClass *.java
+javac -cp ./lib/jfreechart.jar:./lib/gson.jar:./lib/jBCrypt.jar -d ./SatelliteClass -encoding UTF-8 *.java
+
 
 # Execução da classe SatelliteInterface com o JAR do JFreeChart no classpath
-java -cp .:./lib/jfreechart.jar:./SatelliteClass SatelliteInterface
+java -cp .:./lib/jfreechart.jar:./lib/gson.jar:./lib/jBCrypt.jar:./SatelliteClass SatelliteInterface
+
+
 
